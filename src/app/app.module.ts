f21888fc45491daf1391b9f7e504fedd8c765c53
Saúl Laínez <saullainez@hotmail.es>
2021-04-12 07:52:22 -0600

    Componente del mapa y su diseño

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
+import { MapaComponent } from './components/mapa/mapa.component';
 
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapaComponent
   ],
   imports: [
     BrowserModule,
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -5,6 +5,7 @@ import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar'; 
 import { MatButtonModule } from '@angular/material/button'; 
 import { MatProgressBarModule } from '@angular/material/progress-bar'; 
+import { MatCardModule } from '@angular/material/card'; 
 
 
 
@@ -14,12 +15,14 @@ import { MatProgressBarModule } from '@angular/material/progress-bar';
     CommonModule,
     MatToolbarModule,
     MatButtonModule,
-    MatProgressBarModule
+    MatProgressBarModule,
+    MatCardModule
   ],
   exports: [
     MatToolbarModule,
     MatButtonModule,
-    MatProgressBarModule
+    MatProgressBarModule,
+    MatCardModule
   ]
 })
 export class MaterialModule { }
