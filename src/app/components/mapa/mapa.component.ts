9eb65d3dc2fd92c2b1636e0f85659de021a61e37
Saúl Laínez <saullainez@hotmail.es>
2021-04-13 09:23:21 -0600

    Creando marcadores de forma dinámica

--- src/app/components/mapa/mapa.component.ts
+++ src/app/components/mapa/mapa.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Marker } from 'src/app/classes/marcador.class';
 
 @Component({
   selector: 'app-mapa',
@@ -7,12 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapaComponent implements OnInit {
 
+  markers: Marker[] = [];
+
   lat = 51.678418;
   lng = 7.809007;
 
-  constructor() { }
+  constructor() {
+    const newMarker = new Marker(51.678418,  7.809007);
+    this.markers.push(newMarker);
+  }
 
   ngOnInit(): void {
+  }
+
+  addMarker(event){
+    const coords: {lat: number, lng: number} = event.coords;
+    const newMarker = new Marker(coords.lat,  coords.lng);
+    this.markers.push(newMarker);
+    console.log(event);
   }
 
 }
